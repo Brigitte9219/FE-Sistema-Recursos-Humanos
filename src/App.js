290ff78a0caa6fa848a5d3c22c620ed455336fcf
BrigittePadilla <153017836+BrigittePadilla@users.noreply.github.com>
2024-09-12 15:34:41 -0500

    Listar empleados con React y Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
+import ListadoEmpleados from "./empleados/ListadoEmpleados";
 
 function App() {
   return (
-    <div className="container text-center">
-      <h1>Sistema de Recursos humanos</h1>
-    </div>
+    <ListadoEmpleados />
   );
 }
 
